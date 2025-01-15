2e2bb0a7332038058de3e2715074800a1fe9c483
jamshed <32561969+jamshed5@users.noreply.github.com>
2025-01-15 15:49:24 +0100

    personal portfolio
    first assignment

---
+++ html-css/week1/javascrip.js
@@ -0,0 +1,12 @@
+// for tabs
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("development");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(cityName).style.display = "block";  
+}
+
+
+
